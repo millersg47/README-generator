8c9e573c86d9e0d565304fa9377b4f6435d8c7aa
Sophie Miller <millersg47@gmail.com>
2022-04-19 11:17:21 -0700

    adds list of user questions and inquirer prompt function to ask questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the project title?", 
+    "Provide a project description",
+    "Provide installation instructions",
+    "What is the usage case for the project?",
+    "What license is used?",
+    "Provide contribution instructions",
+    "What test instructions are there for the project?",
+    "What is your GitHub username?",
+    "What is your email address?",
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(README, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "title",
+                message: questions[0]
+            },
+            {
+                type: "input",
+                name: "description",
+                message: questions[1]
+            },
+            {
+                type: "input",
+                name: "installation",
+                message: questions[2] 
+            },
+            {
+                type: "input",
+                name: "usage",
+                message: questions[3]
+            },
+            {
+                type: "list",
+                name: "license",
+                message: questions[4],
+                choices: ["MIT", "GNU GPLv3", "Mozilla Public License 2.0", "Apache License 2.0"],
+            },
+            {
+                type: "input",
+                name: "contribution",
+                message: questions[5]
+            },
+            {
+                type: "input",
+                name: "test",
+                message: questions[6]
+            },
+            {
+                type: "input",
+                name: "github",
+                message: questions[7]
+            },
+            {
+                type: "input",
+                name: "email",
+                message: questions[8]
+            }
+        ])
+}
+
 
 // Function call to initialize app
 init();
